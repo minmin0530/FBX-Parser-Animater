ea462e5710a9596454fd8ce49ffb54da16bc927e
izumi_yoshiki <izumiyoshiki@icloud.com>
2017-09-17 08:31:27 +0900

    vertexを頂点ごとに整理

--- FBX_Parser.js
+++ FBX_Parser.js
@@ -9,16 +9,41 @@ class FBX_Parser {
     //#####
     // 頂点
     //#####
+    console.log("Vertex");
+    document.body.innerHTML += "Vertex" + "<br>";
+
     end = this.fbx.indexOf("Vertices", end);
-    begin = this.fbx.lastIndexOf("\n", end);
-    end = this.fbx.indexOf("}", begin);
-    var token = this.fbx.substring(begin, end);
-    console.log(token);
-    document.body.innerHTML += token + "<br>";
-    
+    begin = this.fbx.indexOf("*", end) + 1;
+    end = this.fbx.indexOf(" ", begin);
+
+    //バーテックスの数
+    var VertexNumber = this.fbx.substring(begin, end);
+      
+    end = this.fbx.indexOf("{", end);
+    begin = this.fbx.indexOf(":", end) + 2;
+
+
+    for (var v = 0; v < VertexNumber; ++v) {
+      if (v == VertexNumber - 1) {
+      end = this.fbx.indexOf("\n", begin);
+      } else {
+      end = this.fbx.indexOf(",", begin);
+      }
+      var token = this.fbx.substring(begin, end);
+        
+      console.log(token);
+      document.body.innerHTML += token;
+      if (v % 3 == 2) {
+        document.body.innerHTML += "<br>";
+      }
+      begin = end + 1;
+    }
     //#################
     // 頂点のインデックス
     //#################
+    console.log("PolygonVertexIndex");
+    document.body.innerHTML += "PolygonVertexIndex" + "<br>";
+
     end = this.fbx.indexOf("PolygonVertexIndex", end);
     begin = this.fbx.indexOf("*", end) + 1;
     end = this.fbx.indexOf(" ", begin);
@@ -57,8 +82,6 @@ class FBX_Parser {
       begin = end + 1;
     }
         
-    console.log("PolygonVertexIndex");
-    document.body.innerHTML += "PolygonVertexIndex" + "<br>";
   }
   showFBX() {
     console.log(this.fbx);
