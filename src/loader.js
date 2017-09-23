3c2ef4c3b80107ea72b4ac5d151e7e112d45944d
izumi_yoshiki <izumiyoshiki@icloud.com>
2017-09-23 10:42:33 +0900

    XMLHttpRequestをloader.jsに追い出し

---
+++ src/loader.js
@@ -0,0 +1,16 @@
+function ajax(url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', url);
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        resolve(xhr.response); // 成功時
+      } else {
+        reject(new Error(xhr.statusText));  // 404エラーなど
+      } 
+    }
+    xhr.onerror = reject; // urlに問題がある場合など
+    xhr.send();
+  })
+}
+
